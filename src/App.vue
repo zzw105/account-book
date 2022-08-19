19eb9e1843ed21e4dabff4abd4c33223e818c24e
张泽文 <13611782678@163.com>
2022-08-19 13:36:48 +0800

    优化显示

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,9 @@ body {
   background-color: rgb(247, 248, 250);
   font-family: Noto Sans SC !important;
 }
+:root {
+  --van-action-sheet-max-height: 90% !important;
+}
 </style>
 
 <style scoped></style>
